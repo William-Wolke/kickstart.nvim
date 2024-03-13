227f28e3263a5ea92b4c63b700bcda64e7e0630e
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2024-03-13 17:01:33 +0100

    add tailwindcss lsp

--- init.lua
+++ init.lua
@@ -630,7 +630,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         --
-
+        tailwindcss = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
