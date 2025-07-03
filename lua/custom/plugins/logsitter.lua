fcb91b22923d93da3908dbe78cb0f8d34f0d9ca7
William Wolke <60339769+William-Wolke@users.noreply.github.com>
2025-07-03 10:14:01 +0200

    add clear logs in buffer command to logsitter

--- lua/custom/plugins/logsitter.lua
+++ lua/custom/plugins/logsitter.lua
@@ -5,7 +5,10 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.keymap.set('n', '<leader>lg', function()
       require('logsitter').log()
-    end)
+    end, { desc = 'Add [L]o[G]' })
+    vim.keymap.set('n', '<leader>lc', function()
+      require('logsitter').clear_buf()
+    end, { desc = '[L]ogs [C]lear' })
   end,
 })
 
