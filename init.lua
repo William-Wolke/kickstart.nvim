198f533d7a8b9907b358ba16d578bb61bdc4898a
William Wolke <60339769+William-Wolke@users.noreply.github.com>
2025-07-04 11:59:53 +0200

    remove keymaps for tab switch for now

--- init.lua
+++ init.lua
@@ -200,10 +200,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
--- [[ Custom keymaps ]]
-vim.keymap.set('n', '<C-L>', '<cmd>tabnext', { desc = 'Change to next tab' })
-vim.keymap.set('n', '<C-H>', '<cmd>tabprev', { desc = 'Change to previous tab' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
